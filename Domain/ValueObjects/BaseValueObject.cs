c63c37d8c34bc6b35c42726eb458a29c868c2a25
Александр <96691037+LeftSlipperr@users.noreply.github.com>
2024-04-08 21:18:28 +0300

    -validation

---
+++ Domain/ValueObjects/BaseValueObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public abstract class BaseValueObject
+    {
+        public override bool Equals(object? obj)
+        {
+            //TODO: Разобраться как сравнить (DeepCompare => DeepClone)
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
